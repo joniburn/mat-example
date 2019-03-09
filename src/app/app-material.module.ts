0647f847b865472e9cdd8b8a81ce1c55ecdabedd
joniburn <joniburn@gmail.com>
2019-03-09 20:23:12 +0900

    Text Inputを追加

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -8,11 +8,13 @@ import { NgModule } from '@angular/core';
 // 名前順でソートすること
 import {
   MatButtonModule,
+  MatInputModule,
 } from '@angular/material';
 
 // 上のimportを名前順でソートしてからコピペ
 const MAT_MODULES = [
   MatButtonModule,
+  MatInputModule,
 ];
 
 @NgModule({
