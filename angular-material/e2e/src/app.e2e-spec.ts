7e4ee1b89d7e8e512b422b43470a777f1a3fbfec
joniburn <joniburn@gmail.com>
2019-03-15 12:31:29 +0900

    アプリ名を変更

--- angular-material/e2e/src/app.e2e-spec.ts
+++ angular-material/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to mat-example!');
+    expect(page.getTitleText()).toEqual('Welcome to angular-material!');
   });
 
   afterEach(async () => {
--- angular-material/src/app/app.component.spec.ts
+++ angular-material/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mat-example'`, () => {
+  it(`should have as title 'angular-material'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('mat-example');
+    expect(app.title).toEqual('angular-material');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mat-example!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-material!');
   });
 });
